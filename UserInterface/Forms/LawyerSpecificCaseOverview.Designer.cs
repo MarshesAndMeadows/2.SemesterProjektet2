9fa3661c815e996186cc24003a9f39c994c87e02
Bolchefar <Frederik@kasten.dk>
2024-04-30 11:27:31 +0200

    Genoprettet forskellige UI elementer i LawyerSpecificCaseOverview

--- UserInterface/Forms/LawyerSpecificCaseOverview.Designer.cs
+++ UserInterface/Forms/LawyerSpecificCaseOverview.Designer.cs
@@ -30,11 +30,27 @@
         {
             panel1 = new Panel();
             btnHelp = new Button();
+            panel2 = new Panel();
+            btnEditClient = new Button();
+            panel3 = new Panel();
+            btnAddService = new Button();
+            dataGridView1 = new DataGridView();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            lblCaseTitle = new Label();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel2);
             panel1.Controls.Add(btnHelp);
             panel1.Location = new Point(-10, -39);
             panel1.Name = "panel1";
@@ -51,6 +67,92 @@
             btnHelp.Text = "?";
             btnHelp.UseVisualStyleBackColor = true;
             // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Controls.Add(panel3);
+            panel2.Location = new Point(839, 107);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(741, 773);
+            panel2.TabIndex = 1;
+            // 
+            // btnEditClient
+            // 
+            btnEditClient.Location = new Point(580, 661);
+            btnEditClient.MaximumSize = new Size(120, 70);
+            btnEditClient.Name = "btnEditClient";
+            btnEditClient.Size = new Size(120, 70);
+            btnEditClient.TabIndex = 0;
+            btnEditClient.Text = "Edit client info";
+            btnEditClient.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonFace;
+            panel3.Controls.Add(btnAddService);
+            panel3.Controls.Add(dataGridView1);
+            panel3.Controls.Add(btnEditClient);
+            panel3.ForeColor = SystemColors.ControlText;
+            panel3.Location = new Point(21, 22);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(703, 734);
+            panel3.TabIndex = 0;
+            // 
+            // btnAddService
+            // 
+            btnAddService.Location = new Point(92, 538);
+            btnAddService.MaximumSize = new Size(120, 70);
+            btnAddService.Name = "btnAddService";
+            btnAddService.Size = new Size(120, 70);
+            btnAddService.TabIndex = 1;
+            btnAddService.Text = "Add service";
+            btnAddService.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(92, 186);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(lblCaseTitle);
+            panel4.Controls.Add(panel5);
+            panel4.Location = new Point(22, 51);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(811, 829);
+            panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = SystemColors.AppWorkspace;
+            panel5.Controls.Add(panel6);
+            panel5.Location = new Point(3, 56);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(805, 302);
+            panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = SystemColors.ButtonFace;
+            panel6.Location = new Point(20, 22);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(766, 258);
+            panel6.TabIndex = 0;
+            // 
+            // lblCaseTitle
+            // 
+            lblCaseTitle.AutoSize = true;
+            lblCaseTitle.Location = new Point(23, 20);
+            lblCaseTitle.Name = "lblCaseTitle";
+            lblCaseTitle.Size = new Size(41, 20);
+            lblCaseTitle.TabIndex = 1;
+            lblCaseTitle.Text = "Title:";
+            // 
             // LawyerSpecificCaseOverview
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -60,6 +162,12 @@
             Name = "LawyerSpecificCaseOverview";
             Text = "LawyerSpecificCaseOverview";
             panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -67,5 +175,14 @@
 
         private Panel panel1;
         private Button btnHelp;
+        private Panel panel2;
+        private Panel panel3;
+        private Button btnEditClient;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Button btnAddService;
+        private DataGridView dataGridView1;
+        private Label lblCaseTitle;
     }
 }
--- UserInterface/Forms/LawyerSpecificCaseOverview.cs
+++ UserInterface/Forms/LawyerSpecificCaseOverview.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UIModels;
 
 namespace UserInterface.Forms
 {
     public partial class LawyerSpecificCaseOverview : Form
     {
+        UICase caseTilForm = new UICase();
         public LawyerSpecificCaseOverview()
         {
             InitializeComponent();
+            caseTilForm.Name = "Casen har et navn";
+            lblCaseTitle.Text = caseTilForm.Name;
         }
     }
 }
