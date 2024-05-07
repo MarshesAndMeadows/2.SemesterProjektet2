66ed6c5037e4cf76ed2859956394f30f0576ebc1
MarshesAndMeadows <khin58394@edu.ucl.dk>
2024-05-07 10:15:06 +0200

    minor ui changes to LawyerCreateCase

--- UserInterface/Forms/LawyerCreateCase.Designer.cs
+++ UserInterface/Forms/LawyerCreateCase.Designer.cs
@@ -31,9 +31,16 @@
             components = new System.ComponentModel.Container();
             panel1 = new Panel();
             panel6 = new Panel();
+            label4 = new Label();
+            dataGridView2 = new DataGridView();
+            dataGridView1 = new DataGridView();
+            lblServices = new Label();
             btnBack = new Button();
             panel3 = new Panel();
             panel5 = new Panel();
+            txtboxSearchClient = new TextBox();
+            dataGridView3 = new DataGridView();
+            uiClientBindingSource = new BindingSource(components);
             lblClientInfo = new Label();
             panel2 = new Panel();
             panel4 = new Panel();
@@ -46,32 +53,18 @@
             dateTimePicker2 = new DateTimePicker();
             dateTimePicker1 = new DateTimePicker();
             lblCaseInfo = new Label();
-            lblServices = new Label();
-            dataGridView1 = new DataGridView();
-            dataGridView2 = new DataGridView();
-            label4 = new Label();
-            dataGridView3 = new DataGridView();
-            txtboxSearchClient = new TextBox();
-            uiClientBindingSource = new BindingSource(components);
-            clientIdDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            firstnameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            lastnameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            sexDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            birthdayDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            emailDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            phoneNumberDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            addressDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
-            subscribedDataGridViewCheckBoxColumn = new DataGridViewCheckBoxColumn();
+            label5 = new Label();
+            textBox2 = new TextBox();
             panel1.SuspendLayout();
             panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panel3.SuspendLayout();
             panel5.SuspendLayout();
-            panel2.SuspendLayout();
-            panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)uiClientBindingSource).BeginInit();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -95,6 +88,46 @@
             panel6.Name = "panel6";
             panel6.Size = new Size(802, 762);
             panel6.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.ActiveBorder;
+            label4.Location = new Point(466, 38);
+            label4.Name = "label4";
+            label4.Size = new Size(333, 20);
+            label4.TabIndex = 3;
+            label4.Text = "This is a list of the services attached to the case ...";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(3, 467);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 51;
+            dataGridView2.RowTemplate.Height = 29;
+            dataGridView2.Size = new Size(796, 292);
+            dataGridView2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 61);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(796, 339);
+            dataGridView1.TabIndex = 1;
+            // 
+            // lblServices
+            // 
+            lblServices.AutoSize = true;
+            lblServices.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            lblServices.Location = new Point(3, 12);
+            lblServices.Name = "lblServices";
+            lblServices.Size = new Size(147, 46);
+            lblServices.TabIndex = 0;
+            lblServices.Text = "Services:";
             // 
             // btnBack
             // 
@@ -124,6 +157,26 @@
             panel5.Size = new Size(744, 292);
             panel5.TabIndex = 1;
             // 
+            // txtboxSearchClient
+            // 
+            txtboxSearchClient.Location = new Point(381, 8);
+            txtboxSearchClient.Name = "txtboxSearchClient";
+            txtboxSearchClient.PlaceholderText = "Search for an existing client ...";
+            txtboxSearchClient.Size = new Size(360, 27);
+            txtboxSearchClient.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.AutoGenerateColumns = false;
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.DataSource = uiClientBindingSource;
+            dataGridView3.Location = new Point(3, 41);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.RowHeadersWidth = 51;
+            dataGridView3.RowTemplate.Height = 29;
+            dataGridView3.Size = new Size(738, 248);
+            dataGridView3.TabIndex = 0;
+            // 
             // lblClientInfo
             // 
             lblClientInfo.AutoSize = true;
@@ -145,6 +198,8 @@
             // 
             // panel4
             // 
+            panel4.Controls.Add(textBox2);
+            panel4.Controls.Add(label5);
             panel4.Controls.Add(label3);
             panel4.Controls.Add(comboboxSelectLawyer);
             panel4.Controls.Add(textBox1);
@@ -170,14 +225,14 @@
             // comboboxSelectLawyer
             // 
             comboboxSelectLawyer.FormattingEnabled = true;
-            comboboxSelectLawyer.Location = new Point(86, 45);
+            comboboxSelectLawyer.Location = new Point(93, 45);
             comboboxSelectLawyer.Name = "comboboxSelectLawyer";
             comboboxSelectLawyer.Size = new Size(200, 28);
             comboboxSelectLawyer.TabIndex = 6;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(86, 6);
+            textBox1.Location = new Point(93, 6);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(200, 27);
             textBox1.TabIndex = 5;
@@ -194,7 +249,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(3, 150);
+            label2.Location = new Point(3, 202);
             label2.Name = "label2";
             label2.Size = new Size(71, 20);
             label2.TabIndex = 3;
@@ -203,7 +258,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(3, 117);
+            label1.Location = new Point(3, 169);
             label1.Name = "label1";
             label1.Size = new Size(77, 20);
             label1.TabIndex = 2;
@@ -211,14 +266,14 @@
             // 
             // dateTimePicker2
             // 
-            dateTimePicker2.Location = new Point(86, 145);
+            dateTimePicker2.Location = new Point(93, 197);
             dateTimePicker2.Name = "dateTimePicker2";
             dateTimePicker2.Size = new Size(250, 27);
             dateTimePicker2.TabIndex = 1;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(86, 112);
+            dateTimePicker1.Location = new Point(93, 164);
             dateTimePicker1.Name = "dateTimePicker1";
             dateTimePicker1.Size = new Size(250, 27);
             dateTimePicker1.TabIndex = 0;
@@ -233,142 +288,22 @@
             lblCaseInfo.TabIndex = 0;
             lblCaseInfo.Text = "Case information:";
             // 
-            // lblServices
+            // label5
             // 
-            lblServices.AutoSize = true;
-            lblServices.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            lblServices.Location = new Point(3, 12);
-            lblServices.Name = "lblServices";
-            lblServices.Size = new Size(147, 46);
-            lblServices.TabIndex = 0;
-            lblServices.Text = "Services:";
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(88, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Description:";
             // 
-            // dataGridView1
+            // textBox2
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(3, 61);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(796, 339);
-            dataGridView1.TabIndex = 1;
-            // 
-            // dataGridView2
-            // 
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Location = new Point(3, 467);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.RowHeadersWidth = 51;
-            dataGridView2.RowTemplate.Height = 29;
-            dataGridView2.Size = new Size(796, 292);
-            dataGridView2.TabIndex = 2;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.ForeColor = SystemColors.ActiveBorder;
-            label4.Location = new Point(466, 38);
-            label4.Name = "label4";
-            label4.Size = new Size(333, 20);
-            label4.TabIndex = 3;
-            label4.Text = "This is a list of the services attached to the case ...";
-            // 
-            // dataGridView3
-            // 
-            dataGridView3.AutoGenerateColumns = false;
-            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView3.Columns.AddRange(new DataGridViewColumn[] { clientIdDataGridViewTextBoxColumn, firstnameDataGridViewTextBoxColumn, lastnameDataGridViewTextBoxColumn, sexDataGridViewTextBoxColumn, birthdayDataGridViewTextBoxColumn, emailDataGridViewTextBoxColumn, phoneNumberDataGridViewTextBoxColumn, addressDataGridViewTextBoxColumn, subscribedDataGridViewCheckBoxColumn });
-            dataGridView3.DataSource = uiClientBindingSource;
-            dataGridView3.Location = new Point(3, 41);
-            dataGridView3.Name = "dataGridView3";
-            dataGridView3.RowHeadersWidth = 51;
-            dataGridView3.RowTemplate.Height = 29;
-            dataGridView3.Size = new Size(738, 248);
-            dataGridView3.TabIndex = 0;
-            // 
-            // txtboxSearchClient
-            // 
-            txtboxSearchClient.Location = new Point(381, 8);
-            txtboxSearchClient.Name = "txtboxSearchClient";
-            txtboxSearchClient.PlaceholderText = "Search for an existing client ...";
-            txtboxSearchClient.Size = new Size(360, 27);
-            txtboxSearchClient.TabIndex = 1;
-            // 
-            // uiClientBindingSource
-            // 
-            uiClientBindingSource.DataSource = typeof(UIModels.UiClient);
-            // 
-            // clientIdDataGridViewTextBoxColumn
-            // 
-            clientIdDataGridViewTextBoxColumn.DataPropertyName = "ClientId";
-            clientIdDataGridViewTextBoxColumn.HeaderText = "ClientId";
-            clientIdDataGridViewTextBoxColumn.MinimumWidth = 6;
-            clientIdDataGridViewTextBoxColumn.Name = "clientIdDataGridViewTextBoxColumn";
-            clientIdDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // firstnameDataGridViewTextBoxColumn
-            // 
-            firstnameDataGridViewTextBoxColumn.DataPropertyName = "Firstname";
-            firstnameDataGridViewTextBoxColumn.HeaderText = "Firstname";
-            firstnameDataGridViewTextBoxColumn.MinimumWidth = 6;
-            firstnameDataGridViewTextBoxColumn.Name = "firstnameDataGridViewTextBoxColumn";
-            firstnameDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // lastnameDataGridViewTextBoxColumn
-            // 
-            lastnameDataGridViewTextBoxColumn.DataPropertyName = "Lastname";
-            lastnameDataGridViewTextBoxColumn.HeaderText = "Lastname";
-            lastnameDataGridViewTextBoxColumn.MinimumWidth = 6;
-            lastnameDataGridViewTextBoxColumn.Name = "lastnameDataGridViewTextBoxColumn";
-            lastnameDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // sexDataGridViewTextBoxColumn
-            // 
-            sexDataGridViewTextBoxColumn.DataPropertyName = "Sex";
-            sexDataGridViewTextBoxColumn.HeaderText = "Sex";
-            sexDataGridViewTextBoxColumn.MinimumWidth = 6;
-            sexDataGridViewTextBoxColumn.Name = "sexDataGridViewTextBoxColumn";
-            sexDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // birthdayDataGridViewTextBoxColumn
-            // 
-            birthdayDataGridViewTextBoxColumn.DataPropertyName = "Birthday";
-            birthdayDataGridViewTextBoxColumn.HeaderText = "Birthday";
-            birthdayDataGridViewTextBoxColumn.MinimumWidth = 6;
-            birthdayDataGridViewTextBoxColumn.Name = "birthdayDataGridViewTextBoxColumn";
-            birthdayDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // emailDataGridViewTextBoxColumn
-            // 
-            emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
-            emailDataGridViewTextBoxColumn.HeaderText = "Email";
-            emailDataGridViewTextBoxColumn.MinimumWidth = 6;
-            emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
-            emailDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // phoneNumberDataGridViewTextBoxColumn
-            // 
-            phoneNumberDataGridViewTextBoxColumn.DataPropertyName = "PhoneNumber";
-            phoneNumberDataGridViewTextBoxColumn.HeaderText = "PhoneNumber";
-            phoneNumberDataGridViewTextBoxColumn.MinimumWidth = 6;
-            phoneNumberDataGridViewTextBoxColumn.Name = "phoneNumberDataGridViewTextBoxColumn";
-            phoneNumberDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // addressDataGridViewTextBoxColumn
-            // 
-            addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
-            addressDataGridViewTextBoxColumn.HeaderText = "Address";
-            addressDataGridViewTextBoxColumn.MinimumWidth = 6;
-            addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
-            addressDataGridViewTextBoxColumn.Width = 125;
-            // 
-            // subscribedDataGridViewCheckBoxColumn
-            // 
-            subscribedDataGridViewCheckBoxColumn.DataPropertyName = "Subscribed";
-            subscribedDataGridViewCheckBoxColumn.HeaderText = "Subscribed";
-            subscribedDataGridViewCheckBoxColumn.MinimumWidth = 6;
-            subscribedDataGridViewCheckBoxColumn.Name = "subscribedDataGridViewCheckBoxColumn";
-            subscribedDataGridViewCheckBoxColumn.Width = 125;
+            textBox2.Location = new Point(93, 89);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(250, 69);
+            textBox2.TabIndex = 9;
             // 
             // LawyerCreateCase
             // 
@@ -381,18 +316,18 @@
             panel1.ResumeLayout(false);
             panel6.ResumeLayout(false);
             panel6.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
             panel5.ResumeLayout(false);
             panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)uiClientBindingSource).EndInit();
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)uiClientBindingSource).EndInit();
             ResumeLayout(false);
         }
 
@@ -431,5 +366,7 @@
         private DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
         private DataGridViewCheckBoxColumn subscribedDataGridViewCheckBoxColumn;
         private BindingSource uiClientBindingSource;
+        private TextBox textBox2;
+        private Label label5;
     }
 }
