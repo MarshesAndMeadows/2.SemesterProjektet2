603bb6d6065fe8459f633847a83b0f72fb42842a
Bolchefar <Frederik@kasten.dk>
2024-05-13 10:43:49 +0200

    Integrated security no, trusted connection yes

--- DataAccess/SqlDbContext.cs
+++ DataAccess/SqlDbContext.cs
@@ -32,7 +32,7 @@ namespace DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-KC4IL1R\\KASTENSQLSERVER; Database=LawHouseDB;Integrated Security=True;")
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KC4IL1R\\KASTENSQLSERVER;Database=LawHouseDB;Trusted_Connection=True;")
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
 
