3ed4fc9a559342a00d96e013c195e138943a8c0a
MarshesAndMeadows <khin58394@edu.ucl.dk>
2024-05-17 10:40:08 +0200

    conn string

--- DataAccess/SqlDbContext.cs
+++ DataAccess/SqlDbContext.cs
@@ -32,7 +32,7 @@ namespace DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=Michael;Database=LawHouseDB;Trusted_Connection=True;")
+            optionsBuilder.UseSqlServer("Server=LAPTOP-SVROVJ19;Database=LawHouseDB;Trusted_Connection=True;")
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
 
