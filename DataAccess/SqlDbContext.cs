8aff20c40991520dcf8fb35a06f9bb359c141140
MarshesAndMeadows <khin58394@edu.ucl.dk>
2024-05-20 09:37:08 +0200

    revert changes

--- DataAccess/SqlDbContext.cs
+++ DataAccess/SqlDbContext.cs
@@ -22,7 +22,7 @@ namespace DataAccess
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=Michael-PC;Database=LawHouseDB;Trusted_Connection=True;")
+            optionsBuilder.UseSqlServer("Server=LAPTOP-SVROVJ19;Database=LawHouseDB;Trusted_Connection=True;")
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
 
